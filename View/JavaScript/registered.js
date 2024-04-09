8a7fc90a20bd840cd3270920036e31c6116bae58
unknown <krlcrystian@gmail.com>
2024-04-09 04:02:28 -0300

    adicionando interatividade com js events

---
+++ View/JavaScript/registered.js
@@ -0,0 +1,16 @@
+const div = document.getElementById('myDiv');
+
+div.addEventListener('mouseover', () => {
+    div.classList.add('highlighted');
+    text();
+});
+
+div.addEventListener('mouseout', () => {
+    div.classList.remove('highlighted');
+    
+});
+
+function text() {
+    const rowtext = document.getElementById('textcontent');
+    rowtext.innerHTML ='Feito por squad 10!';
+}
