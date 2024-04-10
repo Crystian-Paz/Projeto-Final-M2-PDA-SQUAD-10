41cfd31cdf36ba8b53503cd4f74acc2b4760f80b
unknown <krlcrystian@gmail.com>
2024-04-10 20:07:28 -0300

    organizando alguns elementos do css do about

--- View/JavaScript/login.js
+++ View/JavaScript/login.js
@@ -1,11 +1,11 @@
 document.getElementById("loginForm").addEventListener("submit", (ev) => {
     ev.preventDefault();
 
-    const loginInput = document.getElementById("name").value;
+    const loginInput = document.getElementById("name-cad").value;
     const passwordInput = document.getElementById("password-cad").value;
 
     const user = JSON.parse(localStorage.getItem("users")) || [];
-    const registereduser = usuarios.find(user => user.login === loginInput && user.password === passwordInput);
+    const registereduser = user.find(user => user.login === loginInput && user.password === passwordInput);
 
     if (!registereduser) {
         alert("Dados inválidos!");
